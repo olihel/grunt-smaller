fc2820b17fb1b8f67414a6f3c40c06bdff1e0fa1
Oliver Hellebusch <mail@olihel.com>
2013-07-12 11:13:26 +0200

    test for minified js file added

--- test/smaller_test.js
+++ test/smaller_test.js
@@ -6,34 +6,13 @@
  * Released under MIT license (https://raw.github.com/olihel/grunt-smaller/master/LICENSE-MIT)
  */
 
-'use strict';
-
 var fs = require('fs');
 
 var TMP_FOLDER = 'tmp.smaller/';
 var REQUEST_ZIP_FILE = TMP_FOLDER + 'request/smaller.zip';
 var RESPONSE_ZIP_FILE = TMP_FOLDER + 'response/smaller.zip';
+var MIN_JS_FILE = TMP_FOLDER + 'dist/js/app-min.js';
 var MIN_CSS_FILE = TMP_FOLDER + 'dist/css/app-base64-min.css';
-
-/*
-  ======== A Handy Little Nodeunit Reference ========
-  https://github.com/caolan/nodeunit
-
-  Test methods:
-    test.expect(numAssertions)
-    test.done()
-  Test assertions:
-    test.ok(value, [message])
-    test.equal(actual, expected, [message])
-    test.notEqual(actual, expected, [message])
-    test.deepEqual(actual, expected, [message])
-    test.notDeepEqual(actual, expected, [message])
-    test.strictEqual(actual, expected, [message])
-    test.notStrictEqual(actual, expected, [message])
-    test.throws(block, [error], [message])
-    test.doesNotThrow(block, [error], [message])
-    test.ifError(value)
-*/
 
 exports.smaller = {
   setUp: function (done) {
@@ -50,8 +29,9 @@ exports.smaller = {
     test.done();
   },
   checkMinifiedFiles: function (test) {
-    test.expect(1);
-    test.ok(fs.existsSync(MIN_CSS_FILE), 'minified files exist');
+    test.expect(2);
+    test.ok(fs.existsSync(MIN_JS_FILE), 'minified js file exists');
+    test.ok(fs.existsSync(MIN_CSS_FILE), 'minified css file exists');
     test.done();
   }
 };
